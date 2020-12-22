fffadeb2a75b28cb1a4f43e2ea26f456eda87eae
DT <daniel.thomas.ubt@gmail.com>
2020-12-22 15:27:12 +0100

    added spring initializr

---
+++ src/main/java/de/kettcards/web2print/Web2printApplication.java
@@ -0,0 +1,13 @@
+package de.kettcards.web2print;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2printApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2printApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/kettcards/web2print/Web2printApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kettcards.web2print;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2printApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
