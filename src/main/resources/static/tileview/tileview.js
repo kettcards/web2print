480ea5b7fa2aff29089c70e8d384887878f8942b
DT <daniel.thomas.ubt@gmail.com>
2020-12-26 21:00:26 +0100

    tileview pulls now data

--- src/main/resources/static/tileview/tileview.js
+++ src/main/resources/static/tileview/tileview.js
@@ -1,25 +1,34 @@
-var container = $('div.container');
-var pictures = ['https://www.kettcards.de/img/bpanth004.jpg', 'https://www.kettcards.de/img/bpkegu001.jpg', 'https://www.kettcards.de/img/bppeak001.jpg', 'https://www.kettcards.de/img/bpanth004.jpg', 'https://www.kettcards.de/img/bpkegu001.jpg', 'https://www.kettcards.de/img/bppeak001.jpg', 'https://www.kettcards.de/img/bpanth004.jpg', 'https://www.kettcards.de/img/bpkegu001.jpg', 'https://www.kettcards.de/img/bppeak001.jpg', 'https://www.kettcards.de/img/bpanth004.jpg', 'https://www.kettcards.de/img/bpkegu001.jpg', 'https://www.kettcards.de/img/bppeak001.jpg', 'https://www.kettcards.de/img/bpanth004.jpg', 'https://www.kettcards.de/img/bpkegu001.jpg', 'https://www.kettcards.de/img/bppeak001.jpg', 'https://www.kettcards.de/img/bpanth004.jpg', 'https://www.kettcards.de/img/bpkegu001.jpg', 'https://www.kettcards.de/img/bppeak001.jpg', 'https://www.kettcards.de/img/bpanth004.jpg', 'https://www.kettcards.de/img/bpkegu001.jpg', 'https://www.kettcards.de/img/bppeak001.jpg', 'https://www.kettcards.de/img/bpanth004.jpg', 'https://www.kettcards.de/img/bpkegu001.jpg', 'https://www.kettcards.de/img/bppeak001.jpg'];
-var names = ['Weihnachts-Briefpapier: zwei Engelchen auf einer Wolke', 'Weihnachts-Briefpapier: Schönes Fest', 'Weihnachts-Briefpapier: Elche', 'Weihnachts-Briefpapier: zwei Engelchen auf einer Wolke', 'Weihnachts-Briefpapier: Schönes Fest', 'Weihnachts-Briefpapier: Elche', 'Weihnachts-Briefpapier: zwei Engelchen auf einer Wolke', 'Weihnachts-Briefpapier: Schönes Fest', 'Weihnachts-Briefpapier: Elche', 'Weihnachts-Briefpapier: zwei Engelchen auf einer Wolke', 'Weihnachts-Briefpapier: Schönes Fest', 'Weihnachts-Briefpapier: Elche', 'Weihnachts-Briefpapier: zwei Engelchen auf einer Wolke', 'Weihnachts-Briefpapier: Schönes Fest', 'Weihnachts-Briefpapier: Elche', 'Weihnachts-Briefpapier: zwei Engelchen auf einer Wolke', 'Weihnachts-Briefpapier: Schönes Fest', 'Weihnachts-Briefpapier: Elche', 'Weihnachts-Briefpapier: zwei Engelchen auf einer Wolke', 'Weihnachts-Briefpapier: Schönes Fest', 'Weihnachts-Briefpapier: Elche', 'Weihnachts-Briefpapier: zwei Engelchen auf einer Wolke', 'Weihnachts-Briefpapier: Schönes Fest', 'Weihnachts-Briefpapier: Elche'];
-var bestellnr = ['bpanth004', 'bpkegu001', 'bppeak001', 'bpanth004', 'bpkegu001', 'bppeak001', 'bpanth004', 'bpkegu001', 'bppeak001', 'bpanth004', 'bpkegu001', 'bppeak001', 'bpanth004', 'bpkegu001', 'bppeak001', 'bpanth004', 'bpkegu001', 'bppeak001', 'bpanth004', 'bpkegu001', 'bppeak001', 'bpanth004', 'bpkegu001', 'bppeak001'];
+function setCards($container, cards, ref) {
+  $container.empty();
+  for (let c in cards) {
+    let card = cards[c];
+    console.log(card);
+    let name = document.createElement('span');
+    name.className = 'caption';
+    name.textContent = card['name'];
 
-for(i = 0; i < pictures.length; i++) {
-  let item = document.createElement('a');
-  let name = document.createElement('span');
-  let image = document.createElement('img');
-  
-  image.src = pictures[i];
-  
-  name.className = 'caption';
-  name.textContent = names[i];
-  
-  item.href = 'https://bppraktikum.tk/us4_us7/front.php?card=' + bestellnr[i];
-  item.className = 'item';
-  item.append(image);
-  item.append(name);
-  container.append(item);
+    let image = document.createElement('img');
+    image.src = card['thumbnail'];
+
+    let item = document.createElement('a');
+    item.href = ref(card);
+    item.className = 'item';
+
+    item.append(image);
+    item.append(name);
+
+    $container.append(item);
+  }
 }
 
-$('img.item').click(function() {
-  console.log('CLICK');
+
+//TODO remove url
+//TODO add page support
+window.addEventListener('load', function () {
+  $.get("http://localhost:8080/web2print/api/cards", function (response) {
+    console.log(response['page']);
+    setCards($('div.container'),response['content'], function (card) {
+      return 'http://localhost:8080/web2print/editor/' + card['orderId'];
+    });
+  });
 });
