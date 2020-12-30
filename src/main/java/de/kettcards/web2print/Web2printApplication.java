5ef99c3d708555a67afa00f1beabb0f982348ce5
DT <daniel.thomas.ubt@gmail.com>
2020-12-30 22:06:12 +0100

    implicitly disabled webjar/** endpoint for webjars, further endpoints with static resources should be explicitly declared in WebResourceConfiguration.java

--- src/main/java/de/kettcards/web2print/Web2printApplication.java
+++ src/main/java/de/kettcards/web2print/Web2printApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @EnableJpaRepositories
 @ConfigurationPropertiesScan
 @SpringBootApplication
