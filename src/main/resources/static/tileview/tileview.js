0163d739ed93dbde71766c64a20e88bbd9452012
Rennorb <lplucas99@gmail.com>
2021-01-01 23:54:25 +0100

    smol change pls

--- src/main/resources/static/tileview/tileview.js
+++ src/main/resources/static/tileview/tileview.js
@@ -1,17 +1,18 @@
-function setCards($container, cards, ref) {
+const $container = $('div.container');
+
+function setCards(cards, cardId) {
   $container.empty();
-  for (let c in cards) {
-    let card = cards[c];
+  for (let card of cards) {
     console.log(card);
     let name = document.createElement('span');
     name.className = 'caption';
-    name.textContent = card['name'];
+    name.textContent = card.name;
 
     let image = document.createElement('img');
-    image.src = card['thumbnail'];
+    image.src = card.thumbnail;
 
     let item = document.createElement('a');
-    item.href = ref(card);
+    item.href = web2print.links.basePath+'editor/editor.html?card='+cardId;
     item.className = 'item';
 
     item.append(image);
@@ -21,14 +22,10 @@ function setCards($container, cards, ref) {
   }
 }
 
-
-//TODO remove url
 //TODO add page support
 window.addEventListener('load', function () {
-  $.get("http://localhost:8080/web2print/api/cards", function (response) {
-    console.log(response['page']);
-    setCards($('div.container'),response['content'], function (card) {
-      return 'http://localhost:8080/web2print/editor/' + card['orderId'];
-    });
+  $.get(web2print.links.apiUrl+"cards").then(function (response) {
+    console.log(response.page);
+    setCards(response.content, card.orderId);
   });
 });
