f82d834cd5c99df46174f7266894126a4ba47afe
Markus Okon <markus.okon1@gmail.com>
2021-01-05 02:55:26 +0100

    material design now usable

--- src/main/resources/static/struct/struct.js
+++ src/main/resources/static/struct/struct.js
@@ -1,13 +1,15 @@
 const $container = $('div.container');
 
-const inputElement = document.getElementById("input");
-inputElement.addEventListener("change", handleFiles, false);
+const submit = document.getElementById("submit"),
+    input = document.getElementById("input");
 
-fileSelect.addEventListener("click", function (e) {
-    if (fileElem) {
-        fileElem.click();
+submit.addEventListener("click", function (e) {
+    if (input) {
+        input.click();
     }
 }, false);
+
+input.addEventListener("change", handleFiles, false);
 
 function handleFiles() {
     const fileList = this.files;
@@ -23,9 +25,33 @@ function handleFiles() {
         contentType: false
     });
 
-    req.then(function(response) {
+    req.then(function (response) {
         console.log(response)
-    }, function(xhr) {
+    }, function (xhr) {
         console.error('failed to fetch xhr', xhr)
     })
 }
+
+$(window).bind("hashchange", function () {
+    let Import = document.getElementById("Import");
+    let Seitenformat = document.getElementById("Seitenformat");
+    let Motive = document.getElementById("Motive");
+    console.log(location.hash);
+    switch (location.hash) {
+        case "#import":
+            Import.style.display = "inline";
+            Seitenformat.style.display = "none";
+            Motive.style.display = "none";
+            break;
+        case "#ov-format":
+            Import.style.display = "none";
+            Seitenformat.style.display = "inline";
+            Motive.style.display = "none";
+            break;
+        case "#ov-motive":
+            Import.style.display = "none";
+            Seitenformat.style.display = "nonoe";
+            Motive.style.display = "inline";
+            break;
+    }
+});
