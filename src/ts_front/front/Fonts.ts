4f1e60a5cae2480bf49820271d2814f0931eb29f
Lucas Broenner <lucas.broenner@stud.tu-darmstadt.de>
2021-02-12 22:56:17 +0100

    Ts

---
+++ src/ts_front/front/Fonts.ts
@@ -0,0 +1,79 @@
+type FontAttribs = { [key: number]: number; }
+interface FontsObj {
+  defaultFont : string;
+  FontNames   : string[];
+  loadFonts(fontNames : string[]) : JQuery[];
+  FontStyleValues  : { [p: string]: number; };
+  FontAttributeMap : { [key: string]: FontAttribs; };
+
+  beginLoadFont(name: string) : void;
+  loadFont(font : Font) : void;
+}
+
+interface IFontFace {
+  s  : string;
+  fs : 'normal' | 'italic';
+  fw : number | 'bold';
+  v  : number;
+}
+interface Font {
+  name  : string;
+  faces : IFontFace[];
+}
+
+const Fonts = {
+  FontNames: undefined,
+  defaultFont: undefined,
+  FontStyleValues: {
+    b: 0b001,
+    i: 0b010,
+    u: 0b100
+  },
+  FontAttributeMap: {},
+  loadFonts(fontNames) {
+    Fonts.FontNames = fontNames;
+    let $options = new Array(fontNames.length);
+    for(let i = 0; i < fontNames.length; i++) {
+      const fName = fontNames[i];
+      $options[i] = $('<option value="'+fName+'" style="font-family: '+fName+';">'+fName+'</option>');
+      Fonts.beginLoadFont(fName);
+    }
+
+    //(lucas 18.01.21) todo: be more elegant about this, mbe explicitly spec it ?
+    Fonts.defaultFont = fontNames[0];
+
+    return $options;
+  },
+  beginLoadFont: function(name : string) {
+    return $.get(web2print.links.apiUrl+'font/'+name)
+      .then(Fonts.loadFont)
+      .catch(function(e) {
+        alert('[error] could not load font: '+JSON.stringify(e));
+      });
+  },
+  // (lucas 04.01.21)
+  // compat: this might need to use another api, coverage is 93% but that has to mean nothing
+  loadFont: function(font : Font) {
+    let attribs = {};
+    let promises = new Array(font.faces.length);
+    for(let i = 0; i < font.faces.length; i++) {
+      const face = font.faces[i];
+      promises[i] = new FontFace(font.name, 'url('+web2print.links.fontUrl+face.s+')', {
+        style: face.fs,
+        weight: String(face.fw)
+      })
+        .load()
+        .then(function(f){
+          document.fonts.add(f);
+          //todo dont just use b = bold, set the fontweights explicitly
+          attribs[face.v] = face.fw;
+        })
+        .catch(function(e) {
+          alert('[error] could not load font: '+JSON.stringify(e));
+        });
+    }
+    return Promise.allSettled(promises).then(function() {
+      Fonts.FontAttributeMap[font.name] = attribs;
+    });
+  },
+} as FontsObj;
---
+++ src/ts_front/front/UI.ts
@@ -0,0 +1,58 @@
+/// <reference path="./front.ts" />
+/// <reference path="./serialization.ts" />
+/// <reference path="./textHandlers.ts" />
+
+$('.addElBtn').click(hAddElClick);
+
+$("#resize").mousedown(function(e){
+  state.resizing = true;
+});
+
+$("#logoRotation").change(function(e){
+  state.target.css('transform', 'rotate('+ $(this).val()+'deg)');
+}).mouseup(stopPropagation);
+
+$(".alignmentBtn").click(function(){
+  state.target.css('text-align', $(this).val() as string);
+}).mouseup(stopPropagation);
+
+$(".fontTypeButton").click(hChangeFontType).mouseup(stopPropagation);
+
+$('#moveBtn').mousedown(function(){
+  state.dragging = true;
+});
+
+$('#submitBtn').click(serialize);
+
+const $fontSelect = $<HTMLSelectElement>('#fontSelect')
+  .mouseup(stopPropagation)
+  .change(hFontChanged);
+
+const $fontSizeSelect = $<HTMLInputElement>('#fontSizeSelect')
+  .mousedown(function(e){
+    const s = getSel();
+    if(s.rangeCount === 1)
+      state.range = s.getRangeAt(0).cloneRange();
+  })
+  .mouseup(stopPropagation)
+  .change(function(e) {
+    const fontSize = e.target.value;
+    const sel = getSel();
+    sel.removeAllRanges();
+    sel.addRange(state.range);
+    makeNodesFromSelection(sel.getRangeAt(0), function(curr) {
+      $(curr).css('font-size', fontSize+'pt');
+    });
+  });
+
+// flipping pages
+$('.right>.nav-btn-inner').click(function() {
+  hPageSwitch(+1);
+});
+$('.left>.nav-btn-inner').click(function() {
+  hPageSwitch(-1);
+});
+
+$('#recenter-btn').click(function() {
+  Editor.fitToContainer();
+});
