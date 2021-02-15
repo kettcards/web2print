5a0448e34967644499cddebf595e2274c8449c13
Sungsoo Yum <sungsoo.yum@stud.tu-darmstadt.de>
2021-02-15 19:08:26 +0100

    Pdf svg

--- src/main/java/de/kettcards/web2print/pdf/ImageBoxData.java
+++ src/main/java/de/kettcards/web2print/pdf/ImageBoxData.java
@@ -1,12 +1,19 @@
 package de.kettcards.web2print.pdf;
 
+import com.kitfox.svg.A;
+import com.kitfox.svg.SVGDiagram;
+import com.kitfox.svg.SVGException;
+import com.kitfox.svg.SVGUniverse;
 import de.kettcards.web2print.storage.Content;
+import de.rototor.pdfbox.graphics2d.PdfBoxGraphics2D;
 import org.apache.pdfbox.pdmodel.graphics.image.JPEGFactory;
 import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.util.Matrix;
+import org.springframework.core.ReactiveAdapterRegistry;
 
 import javax.imageio.ImageIO;
+import java.awt.geom.AffineTransform;
 import java.io.IOException;
 
 /**
@@ -15,6 +22,7 @@ import java.io.IOException;
 public class ImageBoxData extends BoxData {
 
     private final String contentId;
+    float svgOriginalWidth, svgOriginalHeight;
 
     public ImageBoxData(float x, float y, float width, float height, String contentId) {
         super(x, y, width, height);
@@ -27,17 +35,22 @@ public class ImageBoxData extends BoxData {
         if (content == null)
             throw new IOException("unable to resolve referenced content:" + contentId);
         PDImageXObject img = embedImage(doc, content);
-        Matrix m = new Matrix(this.width,0,0,this.height,doc.getXOffset()+this.x,doc.getYOffset()+this.y);
-        doc.stream().drawImage(img,m);
-        //int[] scaledDim = getScaledDimension(img.getWidth(), img.getHeight(), (int) width, (int) height);
-        //doc.stream().drawImage(img, x + doc.getXOffset(), y + doc.getYOffset(), scaledDim[0], scaledDim[1]);
-
+        Matrix m = new Matrix(this.width, 0, 0, this.height, doc.getXOffset() + this.x, doc.getYOffset() + this.y);
+        if (img != null) {
+            doc.stream().drawImage(img, m);
+        } else {
+            SVGUniverse svgUniverse = new SVGUniverse();
+            SVGDiagram diagram = svgUniverse.getDiagram(svgUniverse.loadSVG(content.getFile().toURL()));
+            svgOriginalWidth = diagram.getWidth();
+            svgOriginalHeight = diagram.getHeight();
+            m = calcNewMatrix(m);
+            drawSVG(doc, diagram, m);
+        }
     }
 
     /**
-     *
      * @param document doc
-     * @param content content to embed
+     * @param content  content to embed
      * @return the embedded image object bound to the given doc
      * @throws IOException
      */
@@ -51,39 +64,37 @@ public class ImageBoxData extends BoxData {
             case "image/bmp":
             case "image/png":
                 return LosslessFactory.createFromImage(document, ImageIO.read(content.getInputStream()));
+            case "image/svg+xml":
+                return null;
             default:
                 throw new IOException("unsupported image type: " + type);
         }
     }
 
-    /**
-     *
-     * @param originalWidth image width
-     * @param originalHeight image height
-     * @param boundWidth bound width
-     * @param boundHeight bound height
-     * @return x, y coords for scaled image
-     */
-    private static int[] getScaledDimension(int originalWidth, int originalHeight, int boundWidth, int boundHeight) {
-        int newWidth = originalWidth;
-        int newHeight = originalHeight;
+    private void drawSVG(Document doc, SVGDiagram diagram, Matrix m) throws IOException {
 
-        // first check if we need to scale width
-        if (originalWidth > boundWidth) {
-            //scale width to fit
-            newWidth = boundWidth;
-            //scale height to maintain aspect ratio
-            newHeight = (newWidth * originalHeight) / originalWidth;
+        PdfBoxGraphics2D graphics = new PdfBoxGraphics2D(doc, svgOriginalWidth, svgOriginalHeight);
+
+        try {
+            diagram.render(graphics);
+        } catch (SVGException e) {
+            e.printStackTrace();
+        } finally {
+            graphics.dispose();
         }
 
-        // then check if we need to scale even with the new height
-        if (newHeight > boundHeight) {
-            //scale height to fit instead
-            newHeight = boundHeight;
-            //scale width to maintain aspect ratio
-            newWidth = (newHeight * originalWidth) / originalHeight;
-        }
-        return new int[]{newWidth, newHeight};
+        var form = graphics.getXFormObject();
+
+        AffineTransform transform = m.createAffineTransform();
+        form.setMatrix(transform);
+        doc.stream().drawForm(form);
+
+    }
+
+    private Matrix calcNewMatrix(Matrix m){
+        float newScaleX = m.getScaleX() / svgOriginalWidth;
+        float newScaleY = m.getScaleY() / svgOriginalHeight;
+        return new Matrix(newScaleX,0,0,newScaleY,m.getTranslateX(),m.getTranslateY());
     }
 
 }
