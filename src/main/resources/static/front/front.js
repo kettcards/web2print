2d8cfebc908f9425ed8a18be2ec89c8387f326e7
Markus Okon <markus.okon@stud.tu-darmstadt.de>
2021-02-15 15:01:12 +0100

    Mail vorschlaege

--- src/main/resources/static/front/front.js
+++ src/main/resources/static/front/front.js
@@ -546,7 +546,7 @@ const createFold = function (fold) {
     }
 };
 const RenderStyles = [{
-        name: 'Simple',
+        name: 'Druckbogen',
         condition: function (card) { return true; },
         BgStretchObjs: {
             stretch: {
@@ -603,7 +603,7 @@ const RenderStyles = [{
             rot: 0
         }
     }, {
-        name: 'simple_foldable',
+        name: 'einzelne Seite',
         condition: function (card) {
             const folds = card.cardFormat.folds;
             return folds.length === 1 && folds[0].x1 === folds[0].x2;
@@ -984,7 +984,7 @@ $.get(web2print.links.apiUrl + 'fonts')
     alert('[fatal] something went wrong loading fonts: ' + JSON.stringify(e));
 });
 if (Cookie.getValue('tutorial') !== 'no') {
-    const $tutOver = $('<div style="position:absolute;top:0;left:0;width:100%;height:100%;background-color:rgba(0,0,0,0.66)"><div class="center" style="max-width:70%;max-height:70%;background-color:gray;padding:5px 5px 15px 5px;"><img src="./tutorial.gif" alt="" style="width:100%;height:100%;display:block;"><input type="checkbox" id="dont-show-again" style="margin:10px 2px 0 0;"><label for="dont-show-again">don\'t show again</label><button style="margin:5px 0 0 0;float: right;">Got It</button></div></div>');
+    const $tutOver = $('<div style="position:absolute;top:0;left:0;width:100%;height:100%;background-color:rgba(0,0,0,0.66)"><div class="center" style="max-width:70%;max-height:70%;background-color:gray;padding:5px 5px 15px 5px;"><img src="./tutorial.gif" alt="" style="width:100%;height:100%;display:block;"><input type="checkbox" id="dont-show-again" style="margin:10px 2px 0 0;"><label for="dont-show-again">don\'t show again</label><button id="tutorial-btn" style="margin:5px 0 0 0;float: right;">Got It</button></div></div>');
     const dontShowAgain = $tutOver.find('input')[0];
     $tutOver.find('button').click(function () {
         if (dontShowAgain.checked) {
--- src/ts_front/front/tutorial.ts
+++ src/ts_front/front/tutorial.ts
@@ -1,6 +1,6 @@
 // tutorial
 if(Cookie.getValue('tutorial') !== 'no') {
-  const $tutOver = $('<div style="position:absolute;top:0;left:0;width:100%;height:100%;background-color:rgba(0,0,0,0.66)"><div class="center" style="max-width:70%;max-height:70%;background-color:gray;padding:5px 5px 15px 5px;"><img src="./tutorial.gif" alt="" style="width:100%;height:100%;display:block;"><input type="checkbox" id="dont-show-again" style="margin:10px 2px 0 0;"><label for="dont-show-again">don\'t show again</label><button style="margin:5px 0 0 0;float: right;">Got It</button></div></div>');
+  const $tutOver = $('<div style="position:absolute;top:0;left:0;width:100%;height:100%;background-color:rgba(0,0,0,0.66)"><div class="center" style="max-width:70%;max-height:70%;background-color:gray;padding:5px 5px 15px 5px;"><img src="./tutorial.gif" alt="" style="width:100%;height:100%;display:block;"><input type="checkbox" id="dont-show-again" style="margin:10px 2px 0 0;"><label for="dont-show-again">don\'t show again</label><button id="tutorial-btn" style="margin:5px 0 0 0;float: right;">Got It</button></div></div>');
   const dontShowAgain = <HTMLInputElement>$tutOver.find('input')[0];
   $tutOver.find('button').click(function(){
     if(dontShowAgain.checked) {
