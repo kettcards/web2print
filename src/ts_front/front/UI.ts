297bf46c723c844281dd6ea6e5ea233cf01d9ecb
Markus Okon <markus.okon1@gmail.com>
2021-02-24 14:03:04 +0100

    fixed picker not visible in chrome and changed saveSelection to mousedown

--- src/ts_front/front/UI.ts
+++ src/ts_front/front/UI.ts
@@ -54,8 +54,8 @@ const $colorpicker = $('#colorpicker').change(function(e){
     }
 });
 
-$("#colorWrap").click(function () {
-  Editor.saveSelection();
+$("#colorWrap").mousedown(Editor.saveSelection)
+  .click(function () {
   $colorpicker.trigger("click");
 })
 
