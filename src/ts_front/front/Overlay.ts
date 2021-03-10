28ab662f28ee6ec73f3870a950b1436ac228c685
muetzenjonas <jonasdoff@web.de>
2021-03-10 13:06:06 +0100

    close button position is now better

--- src/ts_front/front/Overlay.ts
+++ src/ts_front/front/Overlay.ts
@@ -13,6 +13,11 @@ class Overlay{
       const This  = Overlay.tutorial;
       if(!This.isLoaded){
         This.$container.load('./tutorial.html', function(){
+          const center = $('#tutCenter');
+          $('#closeTut').css({
+            top: center.offset().top,
+            left: center.offset().left + center.outerWidth(),
+          });
           const dontShowAgain = This.$container.find<HTMLInputElement>('#dont-show-again' as JQuery.Selector)[0];
           dontShowAgain.checked = Cookie.getValue('tutorial') === 'no';
           This.$container.find('button').click(function(){
@@ -38,6 +43,11 @@ class Overlay{
       const This = Overlay.dsgvo;
       if(!This.isLoaded){
         This.$container.load('./datenschutzerklaerung.html', function(){
+          const center = $('#dsgvoCenter');
+          $('#closeDsgvo').css({
+            top: center.offset().top,
+            left: center.offset().left + center.outerWidth(),
+          });
           This.$container.find('button').click(function(){
             This.$container.css('visibility', 'hidden');
           });
