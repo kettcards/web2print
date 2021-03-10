99e0d12633717092b80c70309de60d93fd6d77a8
DT <daniel.thomas.ubt@gmail.com>
2021-03-10 05:13:47 -0800

    added pdf list

--- src/ts_struct/src/app/pdf-list/pdf-list.component.ts
+++ src/ts_struct/src/app/pdf-list/pdf-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Api} from "../lib/api";
 
 @Component({
   selector: 'app-pdf-list',
   templateUrl: './pdf-list.component.html',
-  styleUrls: ['./pdf-list.component.less']
+  styleUrls: ['./pdf-list.component.less'],
+  providers: [Api]
 })
 export class PdfListComponent implements OnInit {
 
-  constructor() { }
+  elements: string[] = [];
+
+  constructor(private api: Api) { }
 
   ngOnInit(): void {
+    this.loadList();
+  }
+
+  loadList(): void {
+    this.api.listPdfs().subscribe(next => {
+      this.elements = next;
+    });
   }
 
 }
