bb3c9e46d4e473bd20845e4c1c95f047eb64a11a
Markus Okon <markus.okon1@gmail.com>
2021-03-17 00:59:36 +0100

    added default work factor 12

--- src/main/java/de/kettcards/web2print/security/SecurityConfiguration.java
+++ src/main/java/de/kettcards/web2print/security/SecurityConfiguration.java
@@ -125,7 +125,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
      */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(12);
     }
 
     /*
