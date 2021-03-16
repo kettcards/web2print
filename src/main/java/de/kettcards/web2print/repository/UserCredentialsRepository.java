31ddbe972e2c5e314b7f13664d502adc35b192aa
Markus Okon <markus.okon1@gmail.com>
2021-03-16 19:10:36 +0100

    fixed bean loading issue

--- src/main/java/de/kettcards/web2print/repository/UserCredentialsRepository.java
+++ src/main/java/de/kettcards/web2print/repository/UserCredentialsRepository.java
@@ -1,10 +1,11 @@
 package de.kettcards.web2print.repository;
 
 import de.kettcards.web2print.model.db.UserCredentials;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserCredentialsRepository {
+public interface UserCredentialsRepository extends JpaRepository <UserCredentials, String> {
 
     UserCredentials findByUsername(String username);
 }
