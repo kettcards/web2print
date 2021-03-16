37b0f3f85a311e4aaa91bbd07e498ef16b75d08b
Markus Okon <markus.okon1@gmail.com>
2021-03-17 00:10:32 +0100

    removed unnecessary import

--- src/main/java/de/kettcards/web2print/Web2printApplication.java
+++ src/main/java/de/kettcards/web2print/Web2printApplication.java
@@ -3,7 +3,6 @@ package de.kettcards.web2print;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
