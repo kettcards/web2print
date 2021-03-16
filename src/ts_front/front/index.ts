8b7262529310c9b7cdfa42b315df47b55d79384f
Markus Okon <markus.okon1@gmail.com>
2021-03-16 14:23:17 +0100

    added notification that user will be redirected

--- src/ts_front/front/index.ts
+++ src/ts_front/front/index.ts
@@ -6,9 +6,9 @@
 $.get(`${web2print.links.apiUrl}card/${Parameters.card}`)
   .then(loadCard)
   .catch(function() {
-    alert(Parameters.card
+    alert((Parameters.card
       ? 'Die Karte "'+Parameters.card+'" konnte nicht geladen werden!'
-      : 'Es wurde keine Karte gefunden!');
+      : 'Es wurde keine Karte gefunden!') + '\nSie werden auf ' + web2print.links.redirectUrl + ' weitergeleitet.');
     location.href = web2print.links.redirectUrl;
   });
 
